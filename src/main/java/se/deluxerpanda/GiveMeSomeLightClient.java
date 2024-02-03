1aa709f62df4c2426815a5fabae3ccbb2762c4d4
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2024-02-03 19:28:45 +0100

    update

--- src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
+++ src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
@@ -2,14 +2,18 @@ package se.deluxerpanda;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class GiveMeSomeLightClient implements ClientModInitializer {
+    private BlockPos lastPlayerPos;
     @Override
     public void onInitializeClient() {
         LOGGER.info("Hello Fabric Client!");
@@ -20,19 +24,55 @@ public class GiveMeSomeLightClient implements ClientModInitializer {
                         | client.player.getMainHandStack().getItem() == Items.LANTERN
                         | client.player.getMainHandStack().getItem() == Items.OCHRE_FROGLIGHT
                         | client.player.getMainHandStack().getItem() == Items.VERDANT_FROGLIGHT
-                        | client.player.getMainHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT
+                        | client.player.getMainHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT) {
 
-                        | client.player.getOffHandStack().getItem() == Items.TORCH
-                        | client.player.getOffHandStack().getItem() == Items.LANTERN
-                        | client.player.getOffHandStack().getItem() == Items.OCHRE_FROGLIGHT
-                        | client.player.getOffHandStack().getItem() == Items.VERDANT_FROGLIGHT
-                        | client.player.getOffHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT	) {
+                    BlockPos playerPos = new BlockPos((int) client.player.getX(), (int) client.player.getY() + 1, (int) client.player.getZ());
+                    if (!playerPos.equals(lastPlayerPos) && !isPlayerInRestrictedArea(client.player)) {
+                        placeBlock(playerPos);
+                        lastPlayerPos = playerPos;
+                    }
+                }else {
+                    if (client.player.getOffHandStack().getItem() == Items.TORCH
+                            | client.player.getOffHandStack().getItem() == Items.LANTERN
+                            | client.player.getOffHandStack().getItem() == Items.OCHRE_FROGLIGHT
+                            | client.player.getOffHandStack().getItem() == Items.VERDANT_FROGLIGHT
+                            | client.player.getOffHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT) {
 
-                    client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1, 0, false, false, false));
-                }else if (client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION))){
-                    client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+                        BlockPos playerPos = new BlockPos((int) client.player.getX(), (int) client.player.getY() + 1, (int) client.player.getZ());
+
+                        if (!playerPos.equals(lastPlayerPos) && !isPlayerInRestrictedArea(client.player)) {
+                            placeBlock(playerPos);
+                            lastPlayerPos = playerPos;
+                        }
+                    }else {
+                        ClientWorld world = MinecraftClient.getInstance().world;
+                        if (lastPlayerPos != null && world.getBlockState(lastPlayerPos).getBlock() == Blocks.LIGHT) {
+                            world.setBlockState(lastPlayerPos, Blocks.AIR.getDefaultState());
+                        }
+                    }
                 }
             }
         });
     }
-}
+    private void placeBlock(BlockPos pos) {
+        // Get the client world
+        ClientWorld world = MinecraftClient.getInstance().world;
+
+        // Place a block at the player's position
+        assert world != null;
+        world.setBlockState(pos, Blocks.LIGHT.getDefaultState());
+
+        if (lastPlayerPos != null && world.getBlockState(lastPlayerPos).getBlock() == Blocks.LIGHT) {
+            world.setBlockState(lastPlayerPos, Blocks.AIR.getDefaultState());
+        }
+    }
+    private boolean isPlayerInRestrictedArea(Entity player) {
+        // Check if the player is in water or lava
+        if (player.isInFluid() || player.isInLava() || player.isTouchingWater()) {
+            return true;
+        }
+        return false;
+    }
+    }
+
+

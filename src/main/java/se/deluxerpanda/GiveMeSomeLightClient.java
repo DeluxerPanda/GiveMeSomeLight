ff494f9226596ef39423b67104178791c377e4ce
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2024-02-03 22:33:53 +0100

    update

--- src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
+++ src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
@@ -6,73 +6,77 @@ import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import static com.mojang.text2speech.Narrator.LOGGER;
-
 public class GiveMeSomeLightClient implements ClientModInitializer {
     private BlockPos lastPlayerPos;
+
     @Override
     public void onInitializeClient() {
         LOGGER.info("Hello Fabric Client!");
-        ClientTickEvents.END_CLIENT_TICK.register(server -> {
-            MinecraftClient client = MinecraftClient.getInstance();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
-                if (client.player.getMainHandStack().getItem() == Items.TORCH
-                        | client.player.getMainHandStack().getItem() == Items.LANTERN
-                        | client.player.getMainHandStack().getItem() == Items.OCHRE_FROGLIGHT
-                        | client.player.getMainHandStack().getItem() == Items.VERDANT_FROGLIGHT
-                        | client.player.getMainHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT) {
+                PlayerEntity player = MinecraftClient.getInstance().player;
+                ClientWorld world = MinecraftClient.getInstance().world;
+                BlockPos playerPos = new BlockPos((int) player.getX(), (int) (player.getY() + player.getEyeHeight(player.getPose())), (int) player.getZ());
+                if (world.getLightLevel(playerPos) <= 10){
 
-                    BlockPos playerPos = new BlockPos((int) client.player.getX(), (int) client.player.getY() + 1, (int) client.player.getZ());
-                    if (!playerPos.equals(lastPlayerPos) && !isPlayerInRestrictedArea(client.player)) {
-                        placeBlock(playerPos);
-                        lastPlayerPos = playerPos;
+
+                Item mainHandItem = client.player.getMainHandStack().getItem();
+                Item offHandItem = client.player.getOffHandStack().getItem();
+
+                if (!playerPos.equals(lastPlayerPos)) {
+                    removeBlock(lastPlayerPos);
+                }
+
+
+                if (isLightItem(mainHandItem) || isLightItem(offHandItem)) {
+                    placeBlock(playerPos);
+                    lastPlayerPos = playerPos;
+                } else {
+                    if (client.world.getBlockState(playerPos).getBlock() == Blocks.LIGHT) {
+                        removeBlock(playerPos);
                     }
-                }else {
-                    if (client.player.getOffHandStack().getItem() == Items.TORCH
-                            | client.player.getOffHandStack().getItem() == Items.LANTERN
-                            | client.player.getOffHandStack().getItem() == Items.OCHRE_FROGLIGHT
-                            | client.player.getOffHandStack().getItem() == Items.VERDANT_FROGLIGHT
-                            | client.player.getOffHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT) {
-
-                        BlockPos playerPos = new BlockPos((int) client.player.getX(), (int) client.player.getY() + 1, (int) client.player.getZ());
-
-                        if (!playerPos.equals(lastPlayerPos) && !isPlayerInRestrictedArea(client.player)) {
-                            placeBlock(playerPos);
-                            lastPlayerPos = playerPos;
-                        }
-                    }else {
-                        ClientWorld world = MinecraftClient.getInstance().world;
-                        if (lastPlayerPos != null && world.getBlockState(lastPlayerPos).getBlock() == Blocks.LIGHT) {
-                            world.setBlockState(lastPlayerPos, Blocks.AIR.getDefaultState());
-                        }
+                }
+            }else {
+                    if (client.world.getBlockState(playerPos).getBlock() == Blocks.LIGHT) {
+                        removeBlock(playerPos);
                     }
                 }
             }
         });
     }
+
     private void placeBlock(BlockPos pos) {
-        // Get the client world
+
+        ClientWorld world = MinecraftClient.getInstance().world;
+        if (world.getBlockState(pos).getBlock() == Blocks.AIR) {
+            world.setBlockState(pos, Blocks.LIGHT.getDefaultState());
+        }
+    }
+
+    private void removeBlock(BlockPos pos) {
         ClientWorld world = MinecraftClient.getInstance().world;
 
-        // Place a block at the player's position
-        assert world != null;
-        world.setBlockState(pos, Blocks.LIGHT.getDefaultState());
-
-        if (lastPlayerPos != null && world.getBlockState(lastPlayerPos).getBlock() == Blocks.LIGHT) {
-            world.setBlockState(lastPlayerPos, Blocks.AIR.getDefaultState());
+        if (pos != null && world.getBlockState(pos).getBlock() == Blocks.LIGHT) {
+            world.setBlockState(pos, Blocks.AIR.getDefaultState());
         }
     }
+
     private boolean isPlayerInRestrictedArea(Entity player) {
-        // Check if the player is in water or lava
-        if (player.isInFluid() || player.isInLava() || player.isTouchingWater()) {
-            return true;
-        }
-        return false;
-    }
+        return player.isInFluid() || player.isInLava() || player.isTouchingWater();
     }
 
-
+    private boolean isLightItem(Item item) {
+        return item == Items.TORCH ||
+                item == Items.LANTERN ||
+                item == Items.OCHRE_FROGLIGHT ||
+                item == Items.VERDANT_FROGLIGHT ||
+                item == Items.PEARLESCENT_FROGLIGHT;
+    }
+}

57d29c589133307cf161dacc6b923997a74b8af6
DeluxPanda <136261250+DeluxPanda@users.noreply.github.com>
2024-02-03 13:23:52 +0100

    update

--- src/main/java/se/deluxerpanda/GiveMeSomeLight.java
+++ src/main/java/se/deluxerpanda/GiveMeSomeLight.java
@@ -4,8 +4,18 @@ import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.Util;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
+++ src/main/java/se/deluxerpanda/GiveMeSomeLightClient.java
@@ -1,10 +1,36 @@
 package se.deluxerpanda;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.Items;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class GiveMeSomeLightClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        LOGGER.info("Hello Fabric Client!");
+        ClientTickEvents.END_CLIENT_TICK.register(server -> {
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (client.player != null) {
+                if (client.player.getMainHandStack().getItem() == Items.TORCH
+                        | client.player.getMainHandStack().getItem() == Items.LANTERN
+                        | client.player.getMainHandStack().getItem() == Items.OCHRE_FROGLIGHT
+                        | client.player.getMainHandStack().getItem() == Items.VERDANT_FROGLIGHT
+                        | client.player.getMainHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT
 
+                        | client.player.getOffHandStack().getItem() == Items.TORCH
+                        | client.player.getOffHandStack().getItem() == Items.LANTERN
+                        | client.player.getOffHandStack().getItem() == Items.OCHRE_FROGLIGHT
+                        | client.player.getOffHandStack().getItem() == Items.VERDANT_FROGLIGHT
+                        | client.player.getOffHandStack().getItem() == Items.PEARLESCENT_FROGLIGHT	) {
+
+                    client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 1, 0, false, false, false));
+                }
+            }
+        });
     }
 }
